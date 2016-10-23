2199e6177973c0042e7dc327fd68216af5a1e1b6
arsh10209 <arsh10209@gmail.com>
2016-10-23 12:39:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,3 +22,21 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+//sql coonection
+var connection = new ActiveXObject("ADODB.Connection") ;
+ 
+var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+ 
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+ 
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+ 
+rs.close;
+connection.close;
