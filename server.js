b682940345b9c4096a0e343aefb4c459acfd72ae
Arvind bhagat <arsh10209@gmail.com>
2016-11-11 23:12:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,5 +17,30 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+//datyabse part
+var pg = require('pg');
+var config = {
+  user: 'arsh10209',
+  database: 'arsh10209',
+  password: 'db-arsh10209-32406', 
+  host: 'localhost',
+  port: 5432, 
+  max: 10, 
+  idleTimeoutMillis: 30000, 
+};
+var pool = new pg.Pool(config);
+pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    done();
 
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+});
 
