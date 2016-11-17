5dcb53aafaf892e2b64258f1bd8c190eacf09c0b
Arvind bhagat <arsh10209@gmail.com>
2016-11-18 02:30:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,3 +30,13 @@ app.listen(8080, function () {
 });
 
 
+pool.connect(connect,function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query("INSERT INTO register(firstname,lastname,email,mobileno) VALUES($1,$2,$3,$4);",
+  [req.body.First_Name,req.body.Last_Name,req.body.Email_Id,req.body.Mobile_Number]);
+    done();
+    res.redirect('/');
+});
+
