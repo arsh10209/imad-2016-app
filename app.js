51133f0ca8d10d4bba9eefa873df70f68ce6b6d2
Arvind bhagat <arsh10209@gmail.com>
2016-11-18 10:03:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var express=require('express'),
+	path=reqiure('path'),
+	bodyParser=reqiure('bosy-parser),
+	cons=reqiure('consolidate'),
+	dust=reqiure('dustjs-helpers'),
+	pg=reqiure('pg'),
+	app=express();
+//dbconnect
+var connect="postgres://arsh10209:123456@localhost/blogging";
+
+app.engine('dust',cons.dust);
+
+app.set('view engine','dust');
+app.set('views',__dirname+'/ui');
+
+app.use(express.ststic(path.join(__dirname,'public)));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',fuction(req,res){
+	console.log('TEST');
+});
+
+
+    
