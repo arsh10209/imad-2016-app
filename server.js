8b77e9e8b71b05e2ae89eb8d2c4ebb78cea10c88
Arvind bhagat <arsh10209@gmail.com>
2016-11-18 10:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,25 +1,44 @@
-var express=require('express'),
-	path=reqiure('path'),
-	bodyParser=reqiure('bosy-parser'),
-	cons=reqiure('consolidate'),
-	dust=reqiure('dustjs-helpers'),
-	pg=reqiure('pg'),
-	app=express();
-//dbconnect
-var connect="postgres://arsh10209:123456@localhost/blogging";
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var pool=require('pg').Pool;
+var connect={
+    user:'arsh10209',
+    database:'arsh10209',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
-app.engine('dust',cons.dust);
-
-app.set('view engine','dust');
-app.set('views',__dirname+'/ui');
-
-app.use(express.ststic(path.join(__dirname,'public')));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+var app = express();
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/ARVIND.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ARVIND.jpg'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
+var pool = new pg.Pool(connect);
+app.post(function(req,res){
+pool.connect(connect,function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query("INSERT INTO register(firstname,lastname,email,mobileno) VALUES($1,$2,$3,$4);",
+  [req.body.First_Name,req.body.Last_Name,req.body.Email_Id,req.body.Mobile_Number]);
+    done();
+    res.redirect('/');
+});
+});
 
