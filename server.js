cbba62119e8f76f071162915d3a8b2002db157ed
Arvind bhagat <arsh10209@gmail.com>
2016-11-26 12:07:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    
+    res.send(createTemplate(article));
+ // res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
@@ -31,7 +33,7 @@ app.get('/test-db',function(req,res){
         }
     });
 });
-*/
+
 app.get('/articles/article',function(req,res){
     pool.query("SELECT * FROM articles where title='"+req.params.articleName+"'",function(err,result){
         if(err){
@@ -46,8 +48,55 @@ app.get('/articles/article',function(req,res){
         }
     });
 });
+*/
+var article={
+    title:'My Life My Choice',
+    heading:'My Life My Choice',
+    content:`<div id="supportingText">
+			<div id="explanation">
+			<h3><span>What is Lorem Ipsum?</span></h3>
+			<p class="p1"><span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span></p>
+		</div>
+        <div id="participation">
+			<h3><span>Where can I get some?</span></h3>
+			<p class="p1"><span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</span></p>
+		</div>
 
+		<div id="benefits">
+			<h3><span>Why do we use it?</span></h3>
+			<p class="p1"><span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span></p>
+		</div>
+     </div>`
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate=`
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<title>${title}</title>
+<link rel="stylesheet" type="text/css" media="screen" href="ui/style.css" />
+</head>
+<body background-image="header.jpg">
 
+<div id="container">
+	<div id="intro">
+		<div id="pageHeader">
+			<h1><span>${heading}</span></h1><br/><br/>
+			<h2><span>@arsh10209</span></h2>
+		</div>
+        ${content}
+	</div>
+
+</body>
+</html>
+`;
+}
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
