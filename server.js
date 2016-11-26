ed0ecbc2c4b84e993cd4b57f221917c7e1232f9a
Arvind bhagat <arsh10209@gmail.com>
2016-11-26 12:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var article={
     content:`<div id="supportingText">
 			<div id="explanation">
 			<h3><span>What is Lorem Ipsum?</span></h3>
-			<p class="p1"><span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span></p>
+			<p class="p1"><span>fuck.</span></p>
 		</div>
         <div id="participation">
 			<h3><span>Where can I get some?</span></h3>
