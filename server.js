e8aa14628dd78971bb35ca981b38966f7755e99c
Arvind bhagat <arsh10209@gmail.com>
2016-11-26 13:12:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//var pool=require('pg').Pool;
-
-/*var config={
-    user:'arsh10209',
-    database:'arsh10209',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD,
-};
-*/
-
 var app = express();
 app.use(morgan('combined'));
 
 var article={
     title:'My Life My Choice',
     heading:'My Life My Choice',
-    content:`<div id="supportingText">
-			<div id="explanation">
-			<h3><span>What is Lorem Ipsum?</span></h3>
-			<p class="p1"><span>fuck.</span></p>
-		</div>
-        <div id="participation">
-			<h3><span>Where can I get some?</span></h3>
-			<p class="p1"><span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</span></p>
-		</div>
-
+    content:`
 		<div id="benefits">
-			<h3><span>Why do we use it?</span></h3>
-			<p class="p1"><span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</span></p>
+			<h3><span>HAN BHIGAA DETI HAI</span></h3>
+			<p class="p1"><span>
+			    Han bhigaa deti hai
+                Aaj bhi
+                Mere laboon ko,
+                Teri saanson ki ye nami.
+                 
+                Han rula deti hai
+                Aaj bhi
+                Meri aankoon ko,
+                Teri ye kami.
+                 
+                Han badha deti hai
+                Aaj bhi
+                Meri dhadhkan ko,
+                Teri pyaari si ye hasi.
+                 
+                Han mita deti hai
+                Aaj bhi
+                Mere dukhoon ko,
+                Teri ye khushi.
+                 
+                Han bhula deti hai
+                Aaj bhi
+                Mere gusse ko,
+                Teri itraati ye aavaaz pagli.
+                 
+                Han tadhpaa deti hai
+                Aaj bhi
+                Meri chaahat ko,
+                Teri ye naraz khamoshi.</span></p>
 		</div>
      </div>`
 };
@@ -73,35 +82,6 @@ app.get('/articleone', function (req, res) {
     
    res.send(createTemplate(article));
   });
-
-/*var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-        if(err){
-            res.ststus(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-});
-
-app.get('/articles/article',function(req,res){
-    pool.query("SELECT * FROM articles where title='"+req.params.articleName+"'",function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            if(result.rows.length===0){
-                res.status(404).send('article not found');
-            }else{
-                var articledata=result.row[0];
-                res.send(createTemplate(articledata));
-            }
-        }
-    });
-});
-*/
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
