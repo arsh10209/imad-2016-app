72fc573a086d2dc6f4b6aeb3bf459dbc005ff39b
Arvind bhagat <arsh10209@gmail.com>
2016-11-26 12:10:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,41 +14,6 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-    
-    res.send(createTemplate(article));
- // res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-
-/*var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-        if(err){
-            res.ststus(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-});
-
-app.get('/articles/article',function(req,res){
-    pool.query("SELECT * FROM articles where title='"+req.params.articleName+"'",function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            if(result.rows.length===0){
-                res.status(404).send('article not found');
-            }else{
-                var articledata=result.row[0];
-                res.send(createTemplate(articledata));
-            }
-        }
-    });
-});
-*/
 var article={
     title:'My Life My Choice',
     heading:'My Life My Choice',
@@ -97,6 +62,42 @@ function createTemplate(data){
 </html>
 `;
 }
+
+app.get('/', function (req, res) {
+    
+    res.send(createTemplate(article));
+ // res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+/*var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.ststus(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+
+app.get('/articles/article',function(req,res){
+    pool.query("SELECT * FROM articles where title='"+req.params.articleName+"'",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send('article not found');
+            }else{
+                var articledata=result.row[0];
+                res.send(createTemplate(articledata));
+            }
+        }
+    });
+});
+*/
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
